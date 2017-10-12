0314a9c19c84bc626b3ba4f1dbc1ca41228ec2fc
Chris Spring <chris@nolaspring.net>
2017-10-12 15:32:41 -0400

    pushed some asserts into the ino file

---
+++ gimbal.ino
@@ -0,0 +1,13 @@
+// Chris Spring - 2017
+// creates a module that can read sensor values and transmit them over uart
+
+// should be able to turn modue oleds to red
+// should be able to check and see if default address is open
+// should be able to turn module leds to yellow
+// should be able to respond to messages addressed to this unit
+// should be able to turn module leds green and begin data indication routine
+// should poll a given set of pins on a given frequency
+// should transmit raw data from sensors out via I2C
+// should listen over i2c for interrupts or configuration changes
+// should flash leds red when i2c collisions occurr
+// should provide led feedback of manipulations of inputs
