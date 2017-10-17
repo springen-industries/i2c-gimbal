cf572249b4d808b9e0b1ec415e14d0b2742d6300
Chris Spring <chris@nolaspring.net>
2017-10-16 20:58:52 -0400

    messing with uploading

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,78 @@
 
 
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
 
+// the data pin for the NeoPixels
+int neoPixelPin = 6;
+// How many NeoPixels we will be using, charge accordingly
+int numPixels = 5;
 
-void setup(){
+// Instatiate the NeoPixel from the ibrary
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, neoPixelPin, NEO_GRB + NEO_KHZ800);
+
+// Global RGB values, change to suit your needs
+int r = 0;
+int g = 0;
+int b = 255;
+int delayMills = 66;
+// function that executes whenever data is requested by master
+// this function is registered as an event, see 0et0p(0
+void requestEvent() {strip.show();
+  Wire.write("hello "); // respond with message of 6 bytes
+  for( int i = 0; i < numPixels; i++ )
+      strip.setPixelColor(i, 0, 255, 10);
 
 }
+  // as expected by master
 
-void loop(){
+
+void setup() {
+  strip.begin();  // initialize the strip
+  strip.show();   // make sure it is visible
+  strip.clear();  // Initialize all pixels to 'off'
+  Wire.begin(0x0a);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+}
+
+
+
+void updateLEDs(){
+  //colors for internal LED
+  strip.setPixelColor(0, r, g, b);
+// set the colors for the strip
+  for( int i = 1; i < numPixels; i++ ) {
+      strip.setPixelColor(i, r, 255, b);
+      // show all pixels
+      strip.show();
+      delay(delayMills);
+    }
+    for( int i = 1; i < numPixels; i++ ) {
+        strip.setPixelColor(i, 0, 0, 0);
+        // show all pixels
+        strip.show();
+        delay(delayMills);
+      }
+  delay(delayMills);
+
+  for( int i = numPixels; i> 0; i-- )    {
+      strip.setPixelColor(i, r, 255, b);
+      strip.show();
+      delay(delayMills);
+  }
+
+for( int i = numPixels; i > 0; i-- ) {
+      strip.setPixelColor(i, 0, 0, 0);
+      // show all pixels
+      strip.show();
+      delay(delayMills);
+    }
+  delay(delayMills);
+}
+
+void loop() {
+
+  updateLEDs();
 
 }
