1ad19a7059512d16d1867a350288ad9f548b87a1
Chris Spring <chris@nolaspring.net>
2017-11-19 09:13:15 -0500

    pulled dev-debug statments from code

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ uint8_t buffer[4];
 
 // the data pin for the NeoPixels
 int neoPixelPin = 6;
-// How many NeoPixels we will be using, charge accordingly
+// 0 inside -1-4 top middle,
 int numPixels = 5;
 // Instatiate the NeoPixel from the ibrary
 // create a one pole (RC) lowpass filter
@@ -38,69 +38,29 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(numPixels, neoPixelPin, NEO_GRB + NE
 // Global RGB values, change to suit your needs
 int r = 0;
 int g = 0;
-int b = 255;
+int b = 10;
 int delayMills = 10;
 // function that executes whenever data is requested by master
 // this function is registered as an event, see 0et0p(0
 
 
 void updateLEDs(){
-  // as expected by master
-  //colors for internal LED
   strip.setPixelColor(0, r, g, b);
-  for( int i = 1; i < numPixels; i++ ) {
-    // set the colors for the strip
-      strip.setPixelColor(i, r, 255, b);
-      // show all pixels
-      strip.show();
-      delay(delayMills);
-    }
-    for( int i = 1; i < numPixels; i++ ) {
-        strip.setPixelColor(i, 0, 0, 0);
-        // show all pixels
-        strip.show();
-        delay(delayMills);
-      }
-  delay(delayMills);
-
-  for( int i = numPixels; i> 0; i-- )    {
-      strip.setPixelColor(i, r, 255, b);
-      strip.show();
-      delay(delayMills);
+  // zero led is internal, start at 1
+  for(int i=1;i<5;i++) {
+    int ledg = buffer[i-1];
+    strip.setPixelColor(i,r,ledg,b);
   }
-
-for( int i = numPixels; i > 0; i-- ) {
-      strip.setPixelColor(i, 0, 0, 0);
-      // show all pixels
-      strip.show();
-      delay(delayMills);
-    }
-  delay(delayMills);
+  strip.show();
 }
-
 
 void readValues(){
   stick1.loop();
-  Serial.println("--------");
+  stick2.loop();
   buffer[0] = stick1.getX();
-  Serial.print("Stick1 X:");
-  Serial.println(buffer[0]);
-
-  stick1.loop();
   buffer[1] = stick1.getY();
-  Serial.print("Stick1 Y:");
-  Serial.println(buffer[1]);
-
-  stick2.loop();
   buffer[2] = stick2.getX();
-  Serial.print("stick2 X:");
-  Serial.println(buffer[2]);
-
-  stick2.loop();
   buffer[3] = stick2.getY();
-  Serial.print("stick2 Y:");
-  Serial.println(buffer[3]);
-  Serial.println("------------");
 }
 
 void transmitReadings(){
@@ -109,8 +69,6 @@ void transmitReadings(){
 
 void requestEvent() {
   strip.clear();
-  //strip.setPixelColor(0, 0, 255, 10);
-  //strip.show();
   transmitReadings();
 }
 
@@ -126,5 +84,4 @@ void setup() {
 void loop() {
   updateLEDs();
   readValues();
-
 }
